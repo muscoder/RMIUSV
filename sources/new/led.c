133b181480dcead1708307fd7c635dac79c58ae5
muscoder <mustafatosun90@yahoo.com>
2014-05-14 11:51:15 +0000

    Added DS1820 sensor codes

--- sources/new/led.c
+++ sources/new/led.c
@@ -10,6 +10,7 @@
 
 void initLed(void);
 void setLedsLow(void);
+void turnOnLed(char[]);
 
 int main( void ){
 
@@ -23,20 +24,48 @@ int main( void ){
     static char dataReg;
     static int bitCount = 0;
     char str1[3] = "\0";
+    static char str2[3] = "\0";
 
     scanf("%s", str1);
-    //printf("%s\n", str1);
     if (strcmp(str1, "") != 0){
-      printf("%s\n", str1);
       bitCount++;
+      str2[bitCount] = str1[0];
       if(bitCount == 2){
+	turnOnLed(str2);
 	dataReg = 0;
 	bitCount = 0;
-      }
-      }else;
+      } else;
+    } else;
   }
 
   return 0;
+}
+
+void turnOnLed(char str2[]){
+  char result = 0;
+  result = (str2[1] == '1') ? ((str2[2] == '1') ? 3 : 1) : ((str2[2] == '1') ? 2 : 0) ;
+  if(result == 0){
+    bcm2835_gpio_write(OUT_PIN0, HIGH);
+    bcm2835_gpio_write(OUT_PIN1, LOW);
+    bcm2835_gpio_write(OUT_PIN2, LOW);
+    bcm2835_gpio_write(OUT_PIN3, LOW);
+  } else if(result == 1){
+    bcm2835_gpio_write(OUT_PIN0, LOW);
+    bcm2835_gpio_write(OUT_PIN1, HIGH);
+    bcm2835_gpio_write(OUT_PIN2, LOW);
+    bcm2835_gpio_write(OUT_PIN3, LOW);
+  } else if(result == 2){
+    bcm2835_gpio_write(OUT_PIN0, LOW);
+    bcm2835_gpio_write(OUT_PIN1, LOW);
+    bcm2835_gpio_write(OUT_PIN2, HIGH);
+    bcm2835_gpio_write(OUT_PIN3, LOW);
+  } else if(result == 3){
+    bcm2835_gpio_write(OUT_PIN0, LOW);
+    bcm2835_gpio_write(OUT_PIN1, LOW);
+    bcm2835_gpio_write(OUT_PIN2, LOW);
+    bcm2835_gpio_write(OUT_PIN3, HIGH);
+  } else;
+
 }
 
 void initLed(void){
